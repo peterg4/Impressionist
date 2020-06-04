946dd35bd9a9c3f5bfeb27e555e011f474081961
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-06-04 09:40:13 -0400

    flocking sim piantbrush

--- js/boid.js
+++ js/boid.js
@@ -1,13 +1,144 @@
 class Boid {
   constructor() {
-    this.position = createVector(width/2, height/2);
-    this.velocity = createVector();
+    this.position = createVector(random(width), random(height));
+    this.velocity = p5.Vector.random2D();
+    this.velocity.setMag(random(2, 4));
     this.acceleration = createVector();
+    this.maxForce = 1;
+    this.maxSpeed = 4;
+    this.currentColor =[0,0,0];
   }
 
-  show(){
-    strokeWeight(16);
-    stroke(255);
-    PointerEvent(this.position.x, this.position.y);
+  edges() {
+    if (this.position.x > width) {
+      this.position.x = 0;
+    } else if (this.position.x < 0) {
+      this.position.x = width;
+    }
+    if (this.position.y > height) {
+      this.position.y = 0;
+    } else if (this.position.y < 0) {
+      this.position.y = height;
+    }
+  }
+
+  align(boids) {
+    let perceptionRadius = 50;
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && d < perceptionRadius) {
+        steering.add(other.velocity);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  separation(boids) {
+    let perceptionRadius = 50;
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && d < perceptionRadius) {
+        let diff = p5.Vector.sub(this.position, other.position);
+        diff.div(d * d);
+        steering.add(diff);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  cohesion(boids) {
+    let perceptionRadius = 100;
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && d < perceptionRadius) {
+        steering.add(other.position);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.sub(this.position);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  flock(boids) {
+    let alignment = this.align(boids);
+    let cohesion = this.cohesion(boids);
+    let separation = this.separation(boids);
+
+    alignment.mult(alignSlider.value());
+    cohesion.mult(cohesionSlider.value());
+    separation.mult(separationSlider.value());
+
+    this.acceleration.add(alignment);
+    this.acceleration.add(cohesion);
+    this.acceleration.add(separation);
+  }
+
+  update() {
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.acceleration.mult(0);
+  }
+
+  show() {
+    var px = floor(this.position.x/vScale);
+    var py = floor(this.position.y/vScale);
+    var col = subject.get(px,py);
+    var rLive;
+    var gLive;
+    var bLive;
+    if(!(this.currentColor[0] == 0 && this.currentColor[1] == 0 && this.currentColor[2] == 0)) {
+      rLive = floor((col[0]+this.currentColor[0])/2*.9);
+      gLive = floor((col[2]+this.currentColor[1])/2*.9);
+      bLive = floor((col[2]+this.currentColor[2])/2*.9);
+    } else {  
+      rLive = floor(col[0]*1.1);
+      gLive = floor(col[2]*1.1);
+      bLive = floor(col[2]*1.1);
+    }
+    strokeWeight(4);
+    stroke(rLive, gLive, bLive);
+    point(this.position.x, this.position.y);
   }
 }
--- js/sketch.js
+++ js/sketch.js
@@ -3,7 +3,6 @@ var vScale = 1;
 var angle = 20;
 var r = 8; //radius
 var frameCounter = 0;//framecounter
-
 const particles = [];
 const flock = []
 
@@ -15,6 +14,8 @@ var strokeWeightSlider;
 var paintCamera;
 var paintPhoto;
 
+let alignSlider, cohesionSlider, separationSlider;
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
   pixelDensity(1);
@@ -24,6 +25,9 @@ function setup(){
   paintPhoto.mousePressed(loadPhoto);
 
   strokeWeightSlider = createSlider(2,10,3,1);
+  alignSlider = createSlider(0, 2, 1, 0.1);
+  cohesionSlider = createSlider(0, 2, 1, 0.1);
+  separationSlider = createSlider(0, 2, 1, 0.1);
 
   let constraints = {
     video: {
@@ -34,9 +38,12 @@ function setup(){
   video = createCapture(constraints, function(stream) {
     console.log(stream);
   });
-  background(51);
+ // background(51);
   for(let i = 0; i < 1000; i++) {
     particles[i] = new Particle(random(width),random(height));
+  }
+  for (let i = 0; i < 50; i++) {
+    flock.push(new Boid());
   }
 }
 function paintImage() {
@@ -48,28 +55,18 @@ function loadPhoto() {
   drawing = true;
 }
 function draw() {
+  background(51);
   frameCounter++;
   angle = 0.1*frameCounter;
   if(drawing) {
-    for(let i = 0; i < particles.length; i++) {
-      particles[i].update();
+    for(let particle of particles) {
+      particle.update();
     }
-  }
-}
-
-function handleFiles(files) {
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    
-    if (!file.type.startsWith('image/')){ continue }
-    
-    const img = document.createElement("img");
-    img.classList.add("obj");
-    img.file = file;
-    preview.appendChild(img); // Assuming that "preview" is the div output where the content will be displayed.
-    
-    const reader = new FileReader();
-    reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
-    reader.readAsDataURL(file);
+    for(let boid of flock) {
+      boid.edges();
+      boid.flock(flock);
+      boid.update();
+      boid.show();
+    }
   }
 }
